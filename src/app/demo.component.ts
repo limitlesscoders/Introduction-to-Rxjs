1fabcc4a1282e8fcf1e55dabc7ebfd2306fc3cf2
limitlesscoders <49544214+limitlesscoders@users.noreply.github.com>
2023-01-29 18:16:32 -0700

    Added observables types

--- src/app/demo.component.ts
+++ src/app/demo.component.ts
@@ -26,6 +26,20 @@ import {
       <button mat-raised-button (click)="createObservable()">
         Observables
       </button>
+
+      <button mat-raised-button (click)="createSubject()">
+      Subject
+    </button>
+
+    <button mat-raised-button (click)="createReplaySubject()">
+      Replay Subject
+    </button>
+
+    <button mat-raised-button (click)="createBehaviorSubject()">
+      Behavior Subject
+    </button>
+
+
     </div>
   `,
   styles: [
@@ -54,12 +68,48 @@ export class DemoComponent implements OnInit {
     let observable = new Observable((observer) => {
       observer.next('Message 1');
       observer.next('Message 2');
-      observer.next('MEssage 3');
+      observer.next('Message 3');
     });
 
     //observable.next("444");
     observable.subscribe((value) => {
       console.log('Observer data', value);
     });
+  }
+
+  createSubject() {
+    let subject = new Subject();
+    subject.next('Missed Message');
+    subject.subscribe((value) => {
+      console.log('Subject data', value);
+    });
+    subject.next('New Message 1');
+    subject.next('New Message 2');
+    subject.unsubscribe();
+  }
+
+  createReplaySubject() {
+    let replaySubject = new ReplaySubject();
+    replaySubject.next('This message was caught because of replay');
+    replaySubject.next('This message was caught not once but twice');
+    replaySubject.next('This message was caught not once but thrice');
+    replaySubject.subscribe((value) => {
+      console.log('Replay Subject data', value);
+    });
+    replaySubject.next('Hello world!');
+    replaySubject.unsubscribe();
+  }
+
+  createBehaviorSubject() {
+    let behaviorSubject = new BehaviorSubject(
+      'This mesage has some deafult value'
+    );
+
+    behaviorSubject.subscribe((value) => {
+      console.log('Behavior Subject data', value);
+    });
+    console.log('First message', behaviorSubject.getValue());
+    behaviorSubject.next('Second message');
+    behaviorSubject.unsubscribe();
   }
 }
